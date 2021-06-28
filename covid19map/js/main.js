fc1d07112578521e99c35bdfb7c48a724d07e745
j05t <josef.steppan@edu.fh-joanneum.at>
2021-06-28 10:35:22 +0200

    handle touch events

--- covid19map/js/main.js
+++ covid19map/js/main.js
@@ -256,3 +256,31 @@ document.getElementById('toggleDescription').onclick = function () {
         this.innerText = "Show description"
     }
 }
+
+// https://stackoverflow.com/a/1781750
+function touchHandler(event)
+{
+    let touches = event.changedTouches,
+        first = touches[0],
+        type = "";
+    switch(event.type)
+    {
+        case "touchstart": type = "mousedown"; break;
+        case "touchmove":  type = "mousemove"; break;
+        case "touchend":   type = "mouseup";   break;
+        default:           return;
+    }
+
+    let simulatedEvent = document.createEvent("MouseEvent");
+    simulatedEvent.initMouseEvent(type, true, true, window, 1,
+        first.screenX, first.screenY,
+        first.clientX, first.clientY, false,
+        false, false, false, 0/*left*/, null);
+
+    first.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
+}
+document.addEventListener("touchstart", touchHandler, true);
+document.addEventListener("touchmove", touchHandler, true);
+document.addEventListener("touchend", touchHandler, true);
+document.addEventListener("touchcancel", touchHandler, true);
